2fda3b2d2a46d251d8c9505d84155d75bdc016b9
marius-velluet <marius.velluet@gmail.com>
2026-03-24 14:15:03 +0100

    Delete Message.h

--- Message.h
+++
@@ -1,7 +0,0 @@
-typedef struct {
-    int  server_id;   // 4 chiffres
-    int  place_id;    // 4 chiffres  
-    int  menu_id;     // 4 chiffres
-    int  request_id;  // pour la table de routage
-    char payload[512];
-} Message;
