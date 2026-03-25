4a05c3a6d7c2d326ad1c6db0929863916cacb0fd
adrian <adrian.musat@edu.esiee.fr>
2026-03-25 14:36:00 +0100

    23/05

--- main.c
+++ main.c
@@ -17,28 +17,7 @@
 #define BUFFER_SIZE 256
 
 // --- LOGIQUE DU CLIENT ---
-void run_client() {
-    char input[BUFFER_SIZE];
-    char response[BUFFER_SIZE];
-    
-    fprintf(stderr, "[Client] Prêt. Entrez le code (12 chiffres) : ");
-    if (fgets(input, sizeof(input), stdin) != NULL) {
-        input[strcspn(input, "\n")] = 0; // Nettoyage \n
 
-        // 1. Envoyer au routeur
-        int fd_out = open(C_TO_R, O_WRONLY);
-        write(fd_out, input, strlen(input) + 1);
-        close(fd_out);
-        fprintf(stderr, "[Client] Message envoyé au routeur.\n");
-
-        // 2. Attendre réponse
-        int fd_in = open(R_TO_C, O_RDONLY);
-        read(fd_in, response, BUFFER_SIZE);
-        close(fd_in);
-
-        printf("[Client] Résultat reçu : %s\n", response);
-    }
-}
 
 // --- LOGIQUE DU SERVEUR DE ROUTAGE ---
 void run_router() {
